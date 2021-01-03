be41211892f4793ffc37826d355a9809977b0c89
sonamk15 <sonamk18@navgurukul.org>
2021-01-03 11:32:24 +0530

    add a logout commponent || ignore lock file

---
+++ src/components/Logout/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const Logout = function (){
+    const [logout, setLogout] = useState(false)
+
+    const handleLogout = function(){
+        localStorage.removeItem('userDetails')
+        localStorage.removeItem('token')
+        setLogout(true)
+
+    }
+
+    return(
+        <>
+          {localStorage.getItem("token") ? (
+              <button
+                className="btn btn-outline-primary my-2 my-sm-0"
+                style={{ color: "white" }}
+                type="submit"
+                onClick={handleLogout}
+              >
+                log out
+              </button>
+            ) : (
+              <div>
+                <Link to="/">
+                  <button
+                    type="button"
+                    style={{ marginLeft: "10px" }}
+                    className="btn btn-primary btn-arrow-right "
+                  >
+                    Home
+                  </button>
+                </Link>
+              </div>
+            )}
+            {logout ? <Redirect to="/" /> : null}
+        </>
+
+    )
+}
